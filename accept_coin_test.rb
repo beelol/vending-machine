941975e64dbdb615c106d3e79e9a1aa8d433f156
Bilal Itani <bilalitani1@gmail.com>
2018-04-11 09:15:11 -0400

    Fix typo causing a static rather than instance reference to a vending machine method

--- accept_coin_test.rb
+++ accept_coin_test.rb
@@ -7,7 +7,7 @@ RSpec.describe VendingMachine, "#accept" do
     context "with coin details" do
         it "finds nickels respectively" do
 
-            result = VendingMachine.GetValueByCoinDetails
+            result = vendingMachine.GetValueByCoinDetails
             (CoinConstants.NICKEL_WEIGHT, NICKEL_THICKNESS, NICKEL_DIAMETER)
 
             expect(result).to eq CoinConstants.NICKEL_VALUE
@@ -15,18 +15,18 @@ RSpec.describe VendingMachine, "#accept" do
 
         it "finds dimes respectively" do
 
-            result = VendingMachine.GetValueByCoinDetails
-            (CoinConstants.NICKEL_WEIGHT, NICKEL_THICKNESS, NICKEL_DIAMETER)
+            result = vendingMachine.GetValueByCoinDetails
+            (CoinConstants.DIME_WEIGHT, DIME_THICKNESS, DIME_DIAMETER)
 
-            expect(result).to eq CoinConstants.NICKEL_VALUE
+            expect(result).to eq CoinConstants.DIME_VALUE
         end
 
         it "finds quarters respectively" do
 
-            result = VendingMachine.GetValueByCoinDetails
-            (CoinConstants.NICKEL_WEIGHT, NICKEL_THICKNESS, NICKEL_DIAMETER)
+            result = vendingMachine.GetValueByCoinDetails
+            (CoinConstants.QUARTER_WEIGHT, QUARTER_THICKNESS, QUARTER_DIAMETER)
 
-            expect(result).to eq CoinConstants.NICKEL_VALUE
+            expect(result).to eq CoinConstants.QUARTER_VALUE
         end
     end
 end
