2535f81f08ad697254bef1509e727d63773cdf23
Bilal Itani <bilalitani1@gmail.com>
2018-04-11 08:52:29 -0400

    Add starter files, add consts for coin details

---
+++ coin_constants.rb
@@ -0,0 +1,16 @@
+public class CoinConstants
+    # Weight Constants in Grams
+    @@NICKEL_WEIGHT = 5
+    @@DIME_WEIGHT = 2.268
+    @@QUARTER_WEIGHT = 5.67
+
+    # Size Constants in Millimeters
+    @@NICKEL_THICKNESS = 1.95
+    @@DIME_THICKNESS = 1.35
+    @@QUARTER_THICKNESS = 1.75
+
+    # Diameter Constants in Millimeters
+    @@NICKEL_DIAMETER = 21.2
+    @@DIME_DIAMETER = 17.91
+    @@QUARTER_DIAMETER = 24.26
+end
